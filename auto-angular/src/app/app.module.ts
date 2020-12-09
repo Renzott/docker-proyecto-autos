56b419bb28551b5b9849488a30fe8129a6d8a21d
Renzott <renzottaffairs@gmail.com>
2020-12-08 19:54:32 -0500

    add

--- auto-angular/src/app/app.module.ts
+++ auto-angular/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 
 import { HomeComponent } from './components/home/home.component';
 
-import { AutocompleteLibModule } from 'angular-ng-autocomplete';
 import { BusquedaComponent } from './components/busqueda/busqueda.component';
 import { BlogService } from './services/blog.service';
 import { HttpClientModule } from '@angular/common/http';
@@ -22,7 +21,6 @@ import { RegusueventoService } from './services/regusuevento.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AutocompleteLibModule,
     HttpClientModule
   ],
   providers: [BlogService,AutoService,RegusueventoService],
